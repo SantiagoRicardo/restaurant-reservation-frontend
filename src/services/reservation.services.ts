a1873d247c466ed0f66dc5866ece5e49348037aa
SantiagoRicardo <sricard2@ibero.edu.co>
2024-06-14 15:18:53 -0500

    feat: changes implements in Login, ReservationForm, ReservationTable, reservation.services and App

--- src/services/reservation.services.ts
+++ src/services/reservation.services.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import api from "../constants/services";
 import { Reservation, Token, UserToken } from "./types";
 
@@ -16,7 +17,7 @@ export const getTokenLogin = async (
     });
 
     if (response.status === 200) {
-      console.log("Bearrer Token", response.data as Token);
+      console.log("Bearer Token", response.data as Token);
       return response.data as Token;
     } else {
       console.error("Error en la respuesta del servidor:", response.status);
@@ -40,13 +41,37 @@ export const createReservation = async (reservation: Reservation) => {
 
 export const updateReservation = async (
   id: number,
-  reservation: Omit<Reservation, "id">
+  reservation: Omit<Reservation, "id">,
+  token: string
 ) => {
-  const response = await api.put(`/reservations/${id}`, reservation);
+  const updatedReservation = {
+    ...reservation,
+    age: Number(reservation.age),
+  };
+
+  const response = await api.put(`/reservations/${id}`, updatedReservation, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
   return response.data;
 };
 
-export const deleteReservation = async (id: number) => {
-  const response = await api.delete(`/reservations/${id}`);
-  return response.data;
+export const deleteReservation = async (id: number, token: string) => {
+  try {
+    const response = await api.delete(`/reservations/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response?.status === 403) {
+      alert("Acceso denegado. Solo el gerente puede eliminar reservaciones.");
+      throw new Error("Acceso denegado.");
+    } else {
+      console.error("Error al eliminar la reservación:", error);
+      throw error;
+    }
+  }
 };
